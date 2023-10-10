d4ce6e11d87a63651ae3f3d17a5880674f9a39ae
Ruchika2023 <122255444+Ruchika2023@users.noreply.github.com>
2023-10-10 11:12:12 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class Relational {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100;
+		int b=100;
+		if(a==b) {
+			System.out.println(a);
+		}else {
+			System.out.println(b);
+		}
+	}
+
+}
