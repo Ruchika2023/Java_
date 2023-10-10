88dd11bd0b5dd8bb9aff15c0d15061c158033534
Ruchika2023 <122255444+Ruchika2023@users.noreply.github.com>
2023-10-10 11:09:45 +0530

    Add files via upload

---
+++ Arthmetic.java
@@ -0,0 +1,30 @@
+package Operators;
+
+public class Arthmetic {
+int a=11;
+int b=22;
+int c=a+b;
+
+static int x=88;
+static int y=77;
+static int z=x-y;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int r=30;
+		int s=40;
+		int t=r*s;
+		
+		int j=30;
+		int k=40;
+		int l=j/k;
+		
+		
+Arthmetic ob=new Arthmetic();
+System.out.println(ob.c);
+System.out.println(Arthmetic.z);
+System.out.println(t);
+System.out.println(l);
+	}
+
+}
