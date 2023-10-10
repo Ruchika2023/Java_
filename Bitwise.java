8b5aa0b8673a503ef3644fa233b677fb648521ff
Ruchika2023 <122255444+Ruchika2023@users.noreply.github.com>
2023-10-10 11:10:37 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,19 @@
+package Operators;
+
+public class Bitwise {
+	public static void main(String[] args) {
+		int a=1;
+		int b=2;
+		int c=a|b; //OR
+		int v=a&b; //AND
+		int s=a^b; //XOR
+		int q=~a; //Negation
+		System.out.println(c);
+		System.out.println(v);
+		System.out.println(s);
+		System.out.println(q);
+		
+		
+	}
+
+}
