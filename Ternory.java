d2e6fb1ec08b4d7f4c3664896f184979ed6c6401
Ruchika2023 <122255444+Ruchika2023@users.noreply.github.com>
2023-10-10 11:17:54 +0530

    Add files via upload

---
+++ Ternory.java
@@ -0,0 +1,17 @@
+package Operators;
+
+public class Ternory {
+	int a;
+	String name;
+	float b;
+	char c;
+	public static void main(String[] args) {
+		Ternory ob=new Ternory();
+		System.out.println(ob.a);
+		System.out.println(ob.name);
+		System.out.println(ob.b);
+		System.out.println(ob.c);
+		
+	}
+
+}
