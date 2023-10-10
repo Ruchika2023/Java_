2d5b831cbdaf2968b81a8a2883315640b3750a78
Ruchika2023 <122255444+Ruchika2023@users.noreply.github.com>
2023-10-10 12:20:22 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,20 @@
+package Operators;
+
+public class Add {
+	public static void main(String[] args) {
+		
+	int math=85;
+	int iot=80;
+	int sql=85;
+	int eng=90;
+	int hindi=97;
+	int management=70;
+	float sum=math+iot+sql+eng+hindi+management;
+	System.out.println("Sum="+sum);
+	float percentage=(sum/600)*100;
+	System.out.println("Percentage="+percentage);
+
+			
+	}
+
+}
