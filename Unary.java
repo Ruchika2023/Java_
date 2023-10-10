30dd11b90d2989060a0c58d91aeb584617b2e4c5
Ruchika2023 <122255444+Ruchika2023@users.noreply.github.com>
2023-10-10 11:18:29 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,24 @@
+package Operators;
+
+public class Unary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=++a;
+
+int x=10;
+int c=x++;
+
+int h=10;
+int v=--h;
+
+int l=10;
+int n=l--;
+
+System.out.println(b);
+System.out.println(c);
+System.out.println(v);
+System.out.println(n);	}
+
+}
