f579916d7d6e14f1e48eec58111e9f86258773d0
Ruchika2023 <122255444+Ruchika2023@users.noreply.github.com>
2023-10-10 11:17:17 +0530

    Add files via upload

---
+++ Shift.java
@@ -0,0 +1,12 @@
+package Operators;
+
+public class Shift {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b = a>>1;
+System.out.println(b);
+	}
+
+}
