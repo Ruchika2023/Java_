6fff42c71c77d00073f4198b4e4833c20e183376
Ruchika2023 <122255444+Ruchika2023@users.noreply.github.com>
2023-10-10 11:11:40 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,15 @@
+package Operators;
+
+import java.util.Scanner;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your name");
+String name=sc.nextLine();
+System.out.println(name);
+	}
+
+}
