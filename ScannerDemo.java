7c0c427bd0ea97e3bbbf4c0a8965a276b6d0a76d
Ruchika2023 <122255444+Ruchika2023@users.noreply.github.com>
2023-10-10 11:12:29 +0530

    Add files via upload

---
+++ ScannerDemo.java
@@ -0,0 +1,20 @@
+package Operators;
+import java.util.Scanner;
+public class ScannerDemo {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter first num");
+int a=sc.nextInt();
+System.out.println("Enter the second num");
+int b=sc.nextInt();
+int c=a+b;
+System.out.println("Enter Num");
+float x=4.8f;
+float y=sc.nextFloat();
+System.out.println("Add 2 nums"+c);
+	}
+
+}
