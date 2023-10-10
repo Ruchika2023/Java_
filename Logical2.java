a9f491f51d2bb2749f8e75138491433449fcbae9
Ruchika2023 <122255444+Ruchika2023@users.noreply.github.com>
2023-10-10 11:22:15 +0530

    Add files via upload

---
+++ Logical2.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class Logical2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=50;
+		int b=65;
+		System.out.println(a<60 & a<b);
+
+	}
+
+}
