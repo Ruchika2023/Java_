081b5593c842167889ce8971dce42faaeb4a38c7
Ruchika2023 <122255444+Ruchika2023@users.noreply.github.com>
2023-10-11 10:13:54 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] x= {1,2,3,4,5};
+		System.out.println(x[4]);
+		
+		
+
+	}
+
+}
